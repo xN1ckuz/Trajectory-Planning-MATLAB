1105abbfd85e01327939b283e21309fd6a7f8915
Nicolas Calzone <74825012+xN1ckuz@users.noreply.github.com>
2022-04-20 11:20:14 +0200

    Delete GUI directory

--- GUI/third_grade_polynomial_threecoeff.m
+++
@@ -1,7 +0,0 @@
-function [p] = third_grade_polynomial_threecoeff(ti, tc, coeff_x, coeff_y, coeff_z)
-  t = tc - ti;
-  p(1) = coeff_x(1) + coeff_x(2) * t + coeff_x(3) * (t^2) + coeff_x(4) * (t^3);
-  p(2) = coeff_y(1) + coeff_y(2) * t + coeff_y(3) * (t^2) + coeff_y(4) * (t^3);
-  p(3) = coeff_z(1) + coeff_z(2) * t + coeff_z(3) * (t^2) + coeff_z(4) * (t^3);
-end
-
