584b5bcc96d10670aba746d9a19a394a64721b6f
xN1ckuz <nicolas.calzone.7@gmail.com>
2022-05-01 12:29:16 +0200

    bugfix - v1.2

--- GUI/trajectory_planning_position.m
+++ GUI/trajectory_planning_position.m
@@ -1,13 +1,16 @@
-function [pe] = trajectory_planning_position(A, t, step)
+function [pe,asc_curv] = trajectory_planning_position(A, t, step)
     pe = zeros(1,3);
+    asc_curv = zeros(1,1);
     for i=2:size(A,1)
         si = 0;
         sf = norm(A(i, :)-A(i-1, :));
         coeff_s = polynomialCoefficients(si, sf, 0, 0, t(i-1), t(i));
         pe(end,:) = [];
+        asc_curv(end,:) = [];
         for k=t(i-1):step:t(i)
             s = third_grade_polynomial_onecoeff(t(i-1), k, coeff_s);
             pe = [pe; A(i-1, :) + ((s/norm(A(i, :)-A(i-1, :))) * (A(i, :)-A(i-1, :)))];
+            asc_curv = [asc_curv; s];
         end
     end
 end
