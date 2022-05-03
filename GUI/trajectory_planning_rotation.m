648c0b7802f49a60bd7ce6e9dc3616283f072c65
xN1ckuz <nicolas.calzone.7@gmail.com>
2022-05-03 16:19:07 +0200

    bugfix - v1.3 stable

--- GUI/trajectory_planning_rotation.m
+++ GUI/trajectory_planning_rotation.m
@@ -1,29 +1,25 @@
 function[theta_vector, re, theta_plot] = trajectory_planning_rotation(B, t, step)
     theta_vector = zeros(1);
-    theta_plot = [];
     re = [];
+    theta_plot = [];
     j = 2;
     for i=4:3:size(B,1)
-        previous_matrix = B(i-3:i-1, :);
-        current_matrix = B(i:i+2, :);
-        %THETA -> ANGOLO DI ROTAZIONE
-        theta_previous = acos((previous_matrix(1,1)+previous_matrix(2,2)+previous_matrix(3,3)-1) / 2);
-        theta_current = acos((current_matrix(1,1)+current_matrix(2,2)+current_matrix(3,3)-1) / 2);
-        coeff_theta = polynomialCoefficients(theta_previous, theta_current, 0, 0, t(j-1), t(j));
-        
-        %RE -> ASSE DI ROTAZIONE
-        r_previous = (1/(2*sin(theta_previous))) * ([previous_matrix(3,2)-previous_matrix(2,3), ...
-            previous_matrix(1,3)-previous_matrix(3,1), previous_matrix(2,1)-previous_matrix(1,2)]);
-        r_current = (1/(2*sin(theta_current))) * ([current_matrix(3,2)-current_matrix(2,3), ...
-            current_matrix(1,3)-current_matrix(3,1), current_matrix(2,1)-current_matrix(1,2)]);
-        
+        %R_i = eye(3);
+        R_i = B(i-3:i-1, :);
+        R_f = B(i:i+2, :);
+        R_if = (R_i)'*R_f;
+        theta_i = 0;
+        theta_f = acos((R_if(1,1)+R_if(2,2)+R_if(3,3)-1) / 2);
+        r_i = (1/(2*sin(theta_f))) * ([R_if(3,2)-R_if(2,3), ...
+             R_if(1,3)-R_if(3,1), R_if(2,1)-R_if(1,2)]);
+        coeff_theta = polynomialCoefficients(theta_i, theta_f, 0, 0, t(j-1), t(j));    
         %Variabili per il plot
         if (i == 4)
-            theta_plot = [theta_previous];
-            re = [r_previous];
+            theta_plot = [theta_i];
+            %re = [r_i];
         end
-        theta_plot = [theta_plot;theta_current];
-        re = [re; r_current];
+        theta_plot = [theta_plot;theta_f];
+        re = [re; r_i];
         
         %Cancelliamo il primo elemento così da non avere lo stesso punto
         %due volte
